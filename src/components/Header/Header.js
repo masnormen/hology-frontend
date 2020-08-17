8739efc1bd504597794da472768572ab17c63222
Ragil Hadi Prasetyo <ragilprasetyo310@gmail.com>
2020-08-17 11:16:38 +0700

    Create Card Component

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,12 +1,17 @@
 import React from "react";
 import "./Header.scss";
 
-const Header = ({ children, center = false, size = "r" }) => {
+const Header = ({
+  children,
+  center = false,
+  size = "r",
+  lineWhite = false,
+}) => {
   return (
     <div>
       <span className="header">
         <h1 className={`text ${size} ${center ? "center" : ""}`}>{children}</h1>
-        <hr className="line" align="left" />
+        <hr className={`line ${lineWhite ? "line-white" : ""}`} align="left" />
       </span>
     </div>
   );
