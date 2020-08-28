38013a59c3e06b0417f0c130bc777412fd74c770
Ragil Hadi Prasetyo <ragilprasetyo310@gmail.com>
2020-08-28 14:56:16 +0700

    Style navbar and remove gsap animation

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -2,9 +2,37 @@ import React, { useState, useEffect } from "react";
 import { NavLink } from "react-router-dom";
 import Logo from "../../assets/logotext.png";
 import "./Navbar.scss";
+import { CSSTransition } from "react-transition-group";
+import { GiHamburgerMenu } from "react-icons/gi";
+import { IoMdClose } from "react-icons/io";
 
 function Navbar() {
   const [isTransparent, setIsTrasnparent] = useState(false);
+  const [isNavVisible, setNavVisibility] = useState(false);
+  const [isSmallScreen, setIsSmallScreen] = useState(false);
+
+  useEffect(() => {
+    const mediaQuery = window.matchMedia("(max-width: 800px)");
+    mediaQuery.addListener(handleMediaQueryChange);
+    handleMediaQueryChange(mediaQuery);
+
+    return () => {
+      mediaQuery.removeListener(handleMediaQueryChange);
+    };
+  }, []);
+
+  const handleMediaQueryChange = (mediaQuery) => {
+    if (mediaQuery.matches) {
+      setIsSmallScreen(true);
+    } else {
+      setIsSmallScreen(false);
+    }
+  };
+
+  const toggleNav = () => {
+    setNavVisibility(!isNavVisible);
+  };
+
   useEffect(() => {
     window.addEventListener("scroll", () => {
       if (window.scrollY > 100) {
@@ -15,59 +43,76 @@ function Navbar() {
       window.removeEventListener("scroll");
     };
   }, []);
+
   return (
     <div className={`navbar ${isTransparent ? "navbar-color" : ""}`}>
       <img src={Logo} alt="Logo Hology" className="logo-text" />
-      <div className="options">
-        <div>
-          <NavLink
-            exact
-            activeClassName="navbar__link--active"
-            className="navbar__link"
-            to="/"
-          >
-            Home
-          </NavLink>
-          <NavLink
-            activeClassName="navbar__link--active"
-            className="navbar__link"
-            to="/competition"
-          >
-            Competition
-          </NavLink>
-          <NavLink
-            activeClassName="navbar__link--active"
-            className="navbar__link"
-            to="/goacademy"
-          >
-            Academy
-          </NavLink>
-          <NavLink
-            activeClassName="navbar__link--active"
-            className="navbar__link"
-            to="/event"
-          >
-            Event
-          </NavLink>
+      <CSSTransition
+        in={!isSmallScreen || isNavVisible}
+        timeout={350}
+        classNames="NavAnimation"
+        unmountOnExit
+      >
+        <div className="options">
+          <div className="section-1">
+            <NavLink
+              exact
+              activeClassName="navbar__link--active"
+              className="navbar__link"
+              to="/"
+            >
+              Home
+            </NavLink>
+            <NavLink
+              activeClassName="navbar__link--active"
+              className="navbar__link"
+              to="/competition"
+            >
+              Competition
+            </NavLink>
+            <NavLink
+              activeClassName="navbar__link--active"
+              className="navbar__link"
+              to="/goacademy"
+            >
+              Academy
+            </NavLink>
+            <NavLink
+              activeClassName="navbar__link--active"
+              className="navbar__link"
+              to="/event"
+            >
+              Event
+            </NavLink>
+          </div>
+          <div className="line"></div>
+          <div className="section-2">
+            <NavLink
+              activeClassName="navbar__link--active"
+              className="navbar__link"
+              to="/login"
+            >
+              Login
+            </NavLink>
+            <NavLink
+              activeClassName="navbar__link--active"
+              className="navbar__link"
+              to="/register"
+            >
+              Register
+            </NavLink>
+          </div>
         </div>
-        <div className="line"></div>
-        <div>
-          <NavLink
-            activeClassName="navbar__link--active"
-            className="navbar__link"
-            to="/login"
-          >
-            Login
-          </NavLink>
-          <NavLink
-            activeClassName="navbar__link--active"
-            className="navbar__link"
-            to="/register"
-          >
-            Register
-          </NavLink>
-        </div>
-      </div>
+      </CSSTransition>
+      <button onClick={toggleNav} className="Burger">
+        <span role="img" aria-label="">
+          {!isNavVisible ? (
+            <GiHamburgerMenu className="icon-hamburger" />
+          ) : (
+            <IoMdClose className="icon-hamburger" />
+          )}
+        </span>
+      </button>
     </div>
   );
 }
--- src/views/Home/Home.js
+++ src/views/Home/Home.js
@@ -1,6 +1,4 @@
 import React, { useRef, useEffect } from "react";
-import { useIntersection } from "react-use";
-import gsap, { TimelineLite, TweenMax } from "gsap";
 import Header from "../../components/Header/Header";
 import Button from "../../components/Button/Button";
 import Paragraph from "../../components/Paragraph/Paragraph";
@@ -8,126 +6,13 @@ import "./Home.scss";
 import { Link } from "react-router-dom";
 
 const Home = () => {
-  let title = useRef(null);
-  const description = useRef(null);
-  let tl = new TimelineLite({ delay: 0.1 });
-  const sectionRef1 = useRef(null);
-  const sectionRef2 = useRef(null);
-  const sectionRef3 = useRef(null);
-  const sectionRef4 = useRef(null);
-  const sectionRef5 = useRef(null);
-  const sectionRef6 = useRef(null);
-  const sectionRef7 = useRef(null);
-
-  useEffect(() => {}, [tl]);
-
-  const intersection1 = useIntersection(sectionRef1, {
-    root: null,
-    rootMargin: "0px",
-    threshold: 0.5,
-  });
-  const intersection2 = useIntersection(sectionRef2, {
-    root: null,
-    rootMargin: "0px",
-    threshold: 0.5,
-  });
-  const intersection3 = useIntersection(sectionRef3, {
-    root: null,
-    rootMargin: "0px",
-    threshold: 0.6,
-  });
-  const intersection4 = useIntersection(sectionRef4, {
-    root: null,
-    rootMargin: "0px",
-    threshold: 0.4,
-  });
-  const intersection5 = useIntersection(sectionRef5, {
-    root: null,
-    rootMargin: "0px",
-    threshold: 0.6,
-  });
-  const intersection6 = useIntersection(sectionRef6, {
-    root: null,
-    rootMargin: "0px",
-    threshold: 0.7,
-  });
-  const intersection7 = useIntersection(sectionRef7, {
-    root: null,
-    rootMargin: "0px",
-    threshold: 0.5,
-  });
-  // Animation for fading in
-  const fadeIn = (element) => {
-    gsap.to(element, 1, {
-      opacity: 1,
-      y: -100,
-      ease: "power4.out",
-      stagger: {
-        amount: 0.3,
-      },
-    });
-  };
-  // Animation for fading out
-  const fadeOut = (element) => {
-    gsap.to(element, 1, {
-      opacity: 0,
-      y: -20,
-      ease: "power3.out",
-    });
-  };
-
-  const imageIn = (element) => {
-    gsap.to(element, 1, {
-      opacity: 1,
-      y: -30,
-      ease: "power4.out",
-      stagger: {
-        amount: 0.75,
-      },
-    });
-  };
-
-  const imageOut = (element) => {
-    gsap.to(element, 1, {
-      opacity: 0,
-      y: -20,
-      ease: "power3.out",
-    });
-  };
-  intersection1 && intersection1.intersectionRatio < 0.5
-    ? fadeOut(".fadeIn1")
-    : fadeIn(".fadeIn1");
-
-  intersection2 && intersection2.intersectionRatio < 0.5
-    ? imageOut(".fadeIn2")
-    : imageIn(".fadeIn2");
-
-  intersection3 && intersection3.intersectionRatio < 0.6
-    ? imageOut(".fadeIn3")
-    : imageIn(".fadeIn3");
-
-  intersection4 && intersection4.intersectionRatio < 0.4
-    ? imageOut(".fadeIn4")
-    : imageIn(".fadeIn4");
-
-  intersection5 && intersection5.intersectionRatio < 0.6
-    ? imageOut(".fadeIn5")
-    : imageIn(".fadeIn5");
-
-  intersection6 && intersection6.intersectionRatio < 0.7
-    ? fadeOut(".fadeIn6")
-    : fadeIn(".fadeIn6");
-
-  intersection7 && intersection7.intersectionRatio < 0.5
-    ? imageOut(".fadeIn7")
-    : imageIn(".fadeIn7");
   return (
     <>
       <div className="section-home-full-1">
         <div className="title">
           <Header size="xl">HOLOGY 3.0</Header>
         </div>
-        <div className="description" ref={description}>
+        <div className="description">
           <Paragraph>
             Lorem Ipsum is simply dummy text of the printing and typesetting
             industry. Lorem Ipsum has been the industry's standard dummy text
@@ -141,7 +26,7 @@ const Home = () => {
         <div className="square-box-2"></div>
       </div>
 
-      <div className="section-home-full-2" ref={sectionRef1}>
+      <div className="section-home-full-2">
         <div className="title fadeIn1">
           <Header size="r">What Is The Theme This Year ?</Header>
         </div>
@@ -163,7 +48,7 @@ const Home = () => {
         <div className="square-box-2"></div>
       </div>
 
-      <div className="section-home-full-3" ref={sectionRef2}>
+      <div className="section-home-full-3">
         <div className="mascot-container fadeIn2"></div>
         <div className="text">
           <div className="title fadeIn2">
@@ -186,7 +71,7 @@ const Home = () => {
         </div>
       </div>
 
-      <div className="section-home-full-4" ref={sectionRef3}>
+      <div className="section-home-full-4">
         <div className="title fadeIn3">
           <Header size="r" center>
             COMPETITION
@@ -220,7 +105,7 @@ const Home = () => {
         <div className="square-box-2"></div>
       </div>
 
-      <div className="section-home-full-5" ref={sectionRef4}>
+      <div className="section-home-full-5">
         <div className="title fadeIn4">
           <Header size="r" center>
             Webinar
@@ -246,7 +131,7 @@ const Home = () => {
         <div className="square-box-2"></div>
       </div>
 
-      <div className="section-home-full-6" ref={sectionRef5}>
+      <div className="section-home-full-6">
         <div className="title fadeIn5">
           <Header size="r" center>
             GO ACADEMY
@@ -280,7 +165,7 @@ const Home = () => {
         <div className="square-box-2"></div>
       </div>
 
-      <div className="section-home-full-7" ref={sectionRef6}>
+      <div className="section-home-full-7">
         <div className="title fadeIn6">
           <Header size="r">Core Value HOLOGY 3.0</Header>
         </div>
@@ -298,7 +183,7 @@ const Home = () => {
         <div className="square-box-2"></div>
       </div>
 
-      <div className="section-home-full-8" ref={sectionRef7}>
+      <div className="section-home-full-8">
         <div className="text">
           <div className="title fadeIn7">
             <Header size="r">Gallery Hology 2.0</Header>
