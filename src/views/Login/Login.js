061c04941305c76ad6b390c09a99fdfddb243af8
Ragil Hadi Prasetyo <ragilprasetyo310@gmail.com>
2020-08-29 15:18:47 +0700

    Fixing responsive problem and styling login page

--- src/views/Login/Login.js
+++ src/views/Login/Login.js
@@ -1,44 +1,53 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Login.scss";
 import Fieldinput from "../../components/Field-input/Fieldinput";
 import Paragraph from "../../components/Paragraph/Paragraph";
 import { Link } from "react-router-dom";
 import Button from "../../components/Button/Button";
 import Header from "../../components/Header/Header";
+import Logo from "../../assets/logo.png";
 
 const Login = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
   return (
     <>
       <div className="login-container-full">
-        <div className="form-login-container">
-          <div className="title">
-            <Header size="r" center>
-              Login Hology
-            </Header>
+        <div className="flexbox-login">
+          <div className="logo-container">
+            <img src={Logo} alt="Hology Logo" />
           </div>
-          <Fieldinput
-            label="Email"
-            name="email"
-            type="email"
-            required
-            marbott
-          />
-          <Fieldinput
-            label="Password"
-            name="password"
-            type="password"
-            marbott
-            required
-          />
-          <Button>Login</Button>
-        </div>
-        <div className="register-text">
-          <Paragraph>
-            If you dont have an account Create{" "}
-            <Link className="link-register" to="/register">
-              here
-            </Link>
-          </Paragraph>
+          <div className="form-login-container">
+            <div className="title">
+              <Header size="r" center>
+                Login Hology
+              </Header>
+            </div>
+            <Fieldinput
+              label="Email"
+              name="email"
+              type="email"
+              required
+              marbott
+            />
+            <Fieldinput
+              label="Password"
+              name="password"
+              type="password"
+              marbott
+              required
+            />
+            <Button>Login</Button>
+            <div className="register-text">
+              <Paragraph>
+                If you dont have an account Create{" "}
+                <Link className="link-register" to="/register">
+                  here
+                </Link>
+              </Paragraph>
+            </div>
+          </div>
         </div>
       </div>
     </>
