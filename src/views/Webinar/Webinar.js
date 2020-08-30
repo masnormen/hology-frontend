c29f519c61b8016abaa0c33f209a73a6024f4079
Ragil Hadi Prasetyo <ragilprasetyo310@gmail.com>
2020-08-30 17:13:16 +0700

    Styling Register and event page

---
+++ src/views/Webinar/Webinar.js
@@ -0,0 +1,27 @@
+import React from "react";
+import "./Webinar.scss";
+import Header from "../../components/Header/Header";
+import Paragraph from "../../components/Paragraph/Paragraph";
+
+const Webinar = () => {
+  return (
+    <>
+      <div className="section-webinar-full-1">
+        <div className="title">
+          <Header size="xl" center>
+            Webinar
+          </Header>
+        </div>
+        <div className="tagline">
+          <Paragraph large header>
+            Coming Really Soon For You
+          </Paragraph>
+        </div>
+        <div className="square-box-1"></div>
+        <div className="square-box-2"></div>
+      </div>
+    </>
+  );
+};
+
+export default Webinar;
