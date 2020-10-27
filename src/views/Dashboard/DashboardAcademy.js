0dcbfce29687332f1e65dbf66e07f2224e2f8913
Nourman Hajar <nourmanhajar@gmail.com>
2020-10-27 13:58:46 +0700

    Updated academy user dashboard

--- src/views/Dashboard/DashboardAcademy.js
+++ src/views/Dashboard/DashboardAcademy.js
@@ -1,72 +1,128 @@
-import React, { useEffect, useState } from "react";
-// import Button from "../../components/Button/Button";
+import React, {useEffect, useRef, useState} from "react";
+import Button from "../../components/Button/Button";
 import Paragraph from "../../components/Paragraph/Paragraph";
 import "./DashboardSection.scss";
 import Header from "../../components/Header/Header";
-// import Fieldinput from "../../components/Field-input/Fieldinput";
-//filepond
-// import { FilePond, registerPlugin } from "react-filepond";
-// import "filepond/dist/filepond.min.css";
-// import FilePondPluginImagePreview from "filepond-plugin-image-preview";
-// import FilePondPluginFileValidateType from "filepond-plugin-file-validate-type";
-// import "filepond-plugin-image-preview/dist/filepond-plugin-image-preview.css";
-// registerPlugin(FilePondPluginImagePreview, FilePondPluginFileValidateType);
+import Fieldinput from "../../components/Field-input/Fieldinput";
+import {FilePond, registerPlugin} from "react-filepond";
+import "filepond/dist/filepond.min.css";
+import FilePondPluginImagePreview from "filepond-plugin-image-preview";
+import FilePondPluginFileValidateType from "filepond-plugin-file-validate-type";
+import "filepond-plugin-image-preview/dist/filepond-plugin-image-preview.css";
+import {getAccessToken, getUserData, setUserData} from "../../components/SessionHelper";
+
+registerPlugin(FilePondPluginImagePreview, FilePondPluginFileValidateType);
 
 const DashboardAcademy = () => {
-  // const [isRegisterAcademy, setIsRegisterAcademy] = useState(false);
+  const [isRegisterAcademy, setIsRegisterAcademy] = useState(false);
+  const [noHp, setNoHp] = useState(null);
+  const fileUploader = useRef(null);
+  
+  useEffect(() => {
+    if (!isRegisterAcademy) return;
+    
+    if (fileUploader.current.getFile() == null || noHp == null || noHp === "") {
+      alert("Complete the form!")
+      setIsRegisterAcademy(false)
+      return;
+    }
+    
+    let data = new FormData();
+    data.append("phone", noHp);
+    data.append("resume", fileUploader.current.getFile().file);
+    
+    fetch("https://multazamgsd.com/hology/api/academy", {
+      method: "POST",
+      headers: {
+        Authorization: "Bearer " + getAccessToken
+      },
+      body: data
+    })
+      .then((raw) => {
+        return raw.json();
+      })
+      .then((res) => {
+        if (res["success"]) {
+          let newData = getUserData;
+          newData["academy"] = res.data.academy;
+          setUserData(newData);
+          window.location.reload();
+          console.log("success!");
+        } else {
+          alert("Failed! Please contact IT support");
+        }
+      })
+      .catch((e) => alert("Error! Please contact IT support"));
+    setIsRegisterAcademy(false);
+  }, [isRegisterAcademy]);
+  
   return (
     <div className="dashboard-section-academy">
       <div className="academy">
         <div className="header">
           <Header center size="r">
-            Academy
+            GO-Academy
           </Header>
         </div>
-        <Paragraph header>Coming Soon</Paragraph>
-        {/* {isRegisterAcademy ? (
+        {getUserData != null && getUserData.institution_id !== 174 ? (
+          <div className="description">
+            <Paragraph header>
+              Maaf. Academy hanya untuk mahasiswa Universitas Brawijaya.
+            </Paragraph>
+          </div>
+        ) :
+        getUserData != null && getUserData.academy.length === 0 ? (
+          // NOT REGISTERED
           <>
             <div className="description">
-              <Paragraph header>
-                Upload berkas untuk mendaftar GoAcademy
+              <Paragraph>
+                Nomor HP:
               </Paragraph>
-            </div>
-            <div className="form-container">
-              <p className="label-filepond">Upload CV</p>
-              <div className="filepond-container">
-                <FilePond
-                  maxFiles="2MB"
-                  name="payment_proof"
-                  checkValidity
-                  dropValidation
-                  // server={{
-                  //   process: {
-                  //     withCredentials: false,
-                  //     url: `https://multazamgsd.com/hology/api/teams/${teamData.team_id}/payment-proof`,
-                  //     headers: {
-                  //       Authorization: "Bearer " + getAccessToken,
-                  //     },
-                  //     onerror: (res) =>
-                  //       alert(JSON.parse(res).message.payment_proof[0]),
-                  //   },
-                  // }}
-                  // acceptedFileTypes={["image/png", "image/jpeg"]}
-                  labelIdle='<span class="filepond--label-action">Klik untuk memilih file</span> atau drag-drop ke sini (.pdf max 2MB)'
+              <div className="input-option">
+                <Fieldinput
+                  name="nohp"
+                  type="number"
+                  required
+                  marbott
+                  value={noHp}
+                  onChange={(e) =>
+                    setNoHp(e.target.value)
+                  }
                 />
               </div>
+              <Paragraph>
+                Unggah CV/Résumé:
+              </Paragraph>
+              <br/>
+              <div className="form-container">
+                <div className="filepond-container">
+                  <FilePond
+                    maxFiles="2MB"
+                    name="resume"
+                    checkValidity
+                    dropValidation
+                    allowMultiple={false}
+                    acceptedFileTypes={["application/pdf"]}
+                    ref={fileUploader}
+                    ins
+                    // acceptedFileTypes={["image/png", "image/jpeg"]}
+                    labelIdle='<span class="filepond--label-action">Klik untuk memilih file</span> atau drag-drop ke sini (.pdf max 2MB)'
+                  />
+                </div>
+              </div>
             </div>
+            <br/><br/>
+            <Button onClicked={() => setIsRegisterAcademy(true)}>Daftar Academy</Button>
           </>
         ) : (
           <>
             <div className="description">
               <Paragraph header>
-                Silahkan Daftarkan Diri Anda di GoAcademy
+                Anda telah terdaftar.<br/>Silakan tunggu untuk pengumuman peserta terpilih!
               </Paragraph>
             </div>
-            <Button onClicked={() => setIsRegisterAcademy(true)}>
-              Register
-            </Button>
           </>
-        )} */}
+        )}
       </div>
     </div>
   );
